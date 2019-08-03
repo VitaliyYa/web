310fd8fdfba435cd7c3953078bfcdc5e110693eb
AlexVasilchenko <alexidest@gmail.com>
2019-08-03 23:31:57 +0300

    Add backend app-server

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = environ['QUERY_STRING'].split('&')
+    body = [item + '\r\n' for item in body]
+    start_response(status, headers )
+    return body
