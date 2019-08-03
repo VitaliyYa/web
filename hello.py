84ae4f98274932516c937a87d33380debeb98fce
AlexVasilchenko <alexidest@gmail.com>
2019-08-04 00:40:08 +0300

    Fix start config

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 
 def wsgi_app(environ, start_response):
     status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
+    headers = [('Content-Type', 'text/plain')]
     body = environ['QUERY_STRING'].split('&')
-    body = [item + '\r\n' for item in body]
+    body = [item + "\r\n" for item in body]
     start_response(status, headers)
     return body
