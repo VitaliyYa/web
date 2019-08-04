4334df7ce677a17c6489a129d23b845fafae2569
AlexVasilchenko <alexidest@gmail.com>
2019-08-04 14:33:21 +0300

    Repaste

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,12 @@
 CONFIG = {
-  'mode': 'wsgi',
-  'working_dir': '/home/box/web',
-  'python': '/usr/bin/python',
-  'args': (
-    '--bind=0.0.0.0:8080',
-    '--workers=16',
-    '--timeout=60',
-    'hello:wsgi_app',
-  )
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web',
+    'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+		'--log-level=debug',
+        'hello',
+    ),
 }
