9190c53b990324237467e9eb2b9b89c52540bfb0
AlexVasilchenko <alexidest@gmail.com>
2019-08-04 12:58:11 +0300

    Another try help me

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+gunicorn -с /etc/gunicorn.d/hello.py hello
 #sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
